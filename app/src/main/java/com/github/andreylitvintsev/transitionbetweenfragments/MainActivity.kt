ab935103fc41f12dbe8c7b5cd4174dc5a76bb962
Litvintsev Andrey <hjrths@yandex.ru>
2019-03-10 22:04:43 +0300

    Small UI improvements

--- app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/MainActivity.kt
+++ app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/MainActivity.kt
@@ -2,11 +2,47 @@ package com.github.andreylitvintsev.transitionbetweenfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val fragmentA = FragmentA()
+    private val fragmentB = FragmentB()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        if (savedInstanceState == null) openFragment(fragmentA)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.fragment1 -> {
+                openFragment(fragmentA)
+                true
+            }
+            R.id.fragment2 -> {
+                openFragment(fragmentB)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 
 }
