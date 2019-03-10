18220923e3e5a06d5814934178c8860dc6bc9109
Litvintsev Andrey <hjrths@yandex.ru>
2019-03-10 21:09:19 +0300

    Init project

---
+++ app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/FragmentA.kt
@@ -0,0 +1,6 @@
+package com.github.andreylitvintsev.transitionbetweenfragments
+
+
+class FragmentA : BaseFragment() {
+    override fun getLayoutId(): Int = R.layout.fragment_a
+}
---
+++ app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/FragmentB.kt
@@ -0,0 +1,6 @@
+package com.github.andreylitvintsev.transitionbetweenfragments
+
+
+class FragmentB : BaseFragment() {
+    override fun getLayoutId(): Int = R.layout.fragment_b
+}
