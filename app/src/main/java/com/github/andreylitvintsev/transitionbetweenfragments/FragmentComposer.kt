463a99c33f1f74e447eeb33a15c1b64547e250b1
Andrey Litvintsev <laa@wmr.ru>
2019-03-20 12:20:37 +0300

    Intermediate 'composer' variant 2

--- app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/FragmentComposer.kt
+++ app/src/main/java/com/github/andreylitvintsev/transitionbetweenfragments/FragmentComposer.kt
@@ -9,20 +9,33 @@ private enum class CommandType {
     TRANSACTION, ANIMATION
 }
 
-private class CommandDescriptor(commandType: CommandType, var nextCommand: CommandDescriptor?, command: () -> Unit)
+private class CommandDescriptor(val commandType: CommandType, var nextCommand: CommandDescriptor?, val command: () -> Unit)
 
 class FragmentComposer(
     private val fragmentManager: FragmentManager
 ) {
-
     private val commands = arrayListOf<CommandDescriptor>() // TODO: можно избавиться
+    private var currentCommandIndex = -1
 
     private var currentFragment: BaseFragment? = null
 
     fun add(@IdRes containerViewId: Int, baseFragment: BaseFragment): FragmentComposer {
         commands.add(CommandDescriptor(CommandType.TRANSACTION, null) {
+            currentCommandIndex++
+
             currentFragment = baseFragment
             fragmentManager.beginTransaction().add(containerViewId, baseFragment).commit()
+
+            if (hasNextCommand()) {
+                if (checkNextCommandForType(CommandType.ANIMATION)) {
+                    baseFragment.setOnResumeListener {
+                        nextCommandDescriptor()?.command?.invoke()
+                    }
+                } else {
+                    nextCommandDescriptor()?.command?.invoke()
+                }
+            }
+
             // TODO: нужен механизм выполнения команд со слушателями, запускаем следующую комманду после ее выполнения
 
             // TODO: при транзакциях мы выполняем моментально следующую, но только в случае если следующая комманда не является анимацией.
@@ -33,6 +46,14 @@ class FragmentComposer(
             commands[commands.size - 2].nextCommand = commands[commands.size - 1]
         }
         return this@FragmentComposer
+    }
+
+    private fun nextCommandDescriptor(): CommandDescriptor? = commands.getOrNull(currentCommandIndex + 1)
+
+    private fun hasNextCommand(): Boolean = (commands.size - 1) != currentCommandIndex
+
+    private fun checkNextCommandForType(commandType: CommandType): Boolean {
+        return (hasNextCommand()) && commands[currentCommandIndex + 1].commandType == commandType
     }
 
     fun remove(baseFragment: BaseFragment): FragmentComposer {
